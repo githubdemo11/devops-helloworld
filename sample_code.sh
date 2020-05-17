ae48febd77d06b2ad7bb3f9b2ca3a1b0af102ced
githubdemo11 <64717047+githubdemo11@users.noreply.github.com>
2020-05-17 11:58:30 +0530

    second commit

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 
 echo "This is an not an sample file"
 echo "This is test from jboss Server"
-echo "This is an example of a script"
+echo "This is an code of an example"
 echo "This is running in a build step and it shows changes"
-SUM=$((FIRSTNUMBER + SECONDNUMBER))
+SUM=$((FIRSTNUMBER + THIRDNUMBER))
 echo "$ADD"
